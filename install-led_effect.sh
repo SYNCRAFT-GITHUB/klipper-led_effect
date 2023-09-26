ce48d6a46f56d3bd3c72cf2419a5186298a99c66
Rafael Neuwirth Swierczynski <rafanswi@protonmail.com>
2023-09-26 14:57:44 -0300

    removed moonraker update_manager add-on

--- install-led_effect.sh
+++ install-led_effect.sh
@@ -45,11 +45,6 @@ check_folders()
     fi
     echo "Klipper installation found at $KLIPPER_PATH"
 
-    if [ ! -f "${MOONRAKER_CONFIG_DIR}/moonraker.conf" ]; then
-        echo "[ERROR] Moonraker configuration not found in directory \"$MOONRAKER_CONFIG_DIR\". Exiting"
-        exit -1
-    fi
-    echo "Moonraker configuration found at $MOONRAKER_CONFIG_DIR"
 }
 
 # Link extension to Klipper
@@ -66,25 +61,6 @@ restart_moonraker()
     echo -n "Restarting Moonraker... "
     sudo systemctl restart moonraker
     echo "[OK]"
-}
-
-# Add updater for led_effect to moonraker.conf
-add_updater()
-{
-    echo -e -n "Adding update manager to moonraker.conf... "
-
-    update_section=$(grep -c '\[update_manager led_effect\]' ${MOONRAKER_CONFIG_DIR}/moonraker.conf || true)
-    if [ "${update_section}" -eq 0 ]; then
-        echo -e "\n" >> ${MOONRAKER_CONFIG_DIR}/moonraker.conf
-        while read -r line; do
-            echo -e "${line}" >> ${MOONRAKER_CONFIG_DIR}/moonraker.conf
-        done < "$PWD/file_templates/moonraker_update.txt"
-        echo -e "\n" >> ${MOONRAKER_CONFIG_DIR}/moonraker.conf
-        echo "[OK]"
-        restart_moonraker
-        else
-        echo -e "[update_manager led_effect] already exists in moonraker.conf [SKIPPED]"
-    fi
 }
 
 restart_klipper()
@@ -114,7 +90,6 @@ uninstall()
         echo -n "Uninstalling... "
         rm -f "${KLIPPER_PATH}/klippy/extras/led_effect.py"
         echo "[OK]"
-        echo "You can now remove the [update_manager led_effect] section in your moonraker.conf and delete this directory. Also remove all led_effect configurations from your Klipper configuration."
     else
         echo "led_effect.py not found in \"${KLIPPER_PATH}/klippy/extras/\". Is it installed?"
         echo "[FAILED]"
@@ -137,7 +112,6 @@ check_folders
 stop_klipper
 if [ ! $UNINSTALL ]; then
     link_extension
-    add_updater
 else
     uninstall
 fi
